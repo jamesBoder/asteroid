0fcbebc74011fdf2ef7f8821750c8c7ae4c00da1
jamesboder <james.macean08@gmail.com>
2025-02-26 03:38:49 -0500

    Updates player icon and movement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 # Initialize game
 pygame.init()
@@ -10,22 +11,44 @@ pygame.init()
 # Display Screen
 screen = pygame.display.set_mode((1280, 800))
 pygame.display.set_caption("My Pygame Window")
-                                 
 black = (0, 0, 0)
+
+# Delta Time
+clock = pygame.time.Clock() # Create an instance of the Clock object
+dt = 0 # Initialize delta time variable
+
+# Player
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+# Groups
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+# Add player to groups
+Player.containers = updatable, drawable
+player.add(updatable, drawable)
+
 
 def main():
     print("Starting Asteroids!")
+    
 
 if __name__ == "__main__":
     main()
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
+# Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    pygame.Surface.fill( screen, black)
+    pygame.Surface.fill(screen, black)
+    updatable.update(dt)
+    for draws in drawable:
+        draws.draw(screen)
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+    
+
